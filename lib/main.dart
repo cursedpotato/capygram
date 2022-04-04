13a9c4006cc5785810b25b0c9b29a68615a4b0aa
Daniel <danireysan@gmail.com>
2022-04-04 10:37:18 -0500

    added firebase depencendies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:capygram/responsive/mobile_screen_layout.dart';
 import 'package:capygram/responsive/responsive_layout_screen.dart';
+import 'package:capygram/responsive/web_screen_layout.dart';
 import 'package:capygram/utils/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor
       ),
-      home: ResponsiveLayout(webScreenLayout: webScreenLayout, mobileScreenLayout: mobileScreenLayout),
+      home: const ResponsiveLayout(webScreenLayout: WebScreenLayout(), mobileScreenLayout: MobileScreenLayout()),
     );
   }
 }
