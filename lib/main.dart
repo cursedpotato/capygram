fbd43bc21861309e3d7518e8d5ffff3c65584f75
Daniel <danireysan@gmail.com>
2022-04-04 19:59:14 -0500

    setting up loginscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,43 @@
 import 'package:capygram/responsive/mobile_screen_layout.dart';
 import 'package:capygram/responsive/responsive_layout_screen.dart';
 import 'package:capygram/responsive/web_screen_layout.dart';
+import 'package:capygram/screens/login_screen.dart';
 import 'package:capygram/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBphuNlMBK2rhRsXLo1x8J7KT0tz8jYP5k",
+        appId: "capygram-aae3d",
+        messagingSenderId: "101216335639",
+        storageBucket: "capygram-aae3d.appspot.com",
+        projectId: "1:101216335639:web:7ec5614696637da2dca952",
+      ),
+    );
+  } else {}
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Capygram',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor
-      ),
-      home: const ResponsiveLayout(webScreenLayout: WebScreenLayout(), mobileScreenLayout: MobileScreenLayout()),
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      // home: const ResponsiveLayout(
+      //     webScreenLayout: WebScreenLayout(),
+      //     mobileScreenLayout: MobileScreenLayout()),
+      home: const LoginScreen(),
     );
   }
 }
-
