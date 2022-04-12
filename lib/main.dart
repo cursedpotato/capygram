e51dc91e0b3a357054db257082c39a291734a865
Daniel <danireysan@gmail.com>
2022-04-12 18:07:03 -0500

    added provider to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:capygram/providers/user_providers.dart';
 import 'package:capygram/responsive/mobile_screen_layout.dart';
 import 'package:capygram/responsive/responsive_layout_screen.dart';
 import 'package:capygram/responsive/web_screen_layout.dart';
@@ -7,6 +8,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,41 +32,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Capygram',
-      theme: ThemeData.dark()
-          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      // home: const ResponsiveLayout(
-      //     webScreenLayout: WebScreenLayout(),
-      //     mobileScreenLayout: MobileScreenLayout()),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            if (snapshot.hasData) {
-              return const ResponsiveLayout(
-                webScreenLayout: WebScreenLayout(),
-                mobileScreenLayout: MobileScreenLayout(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('${snapshot.error}'),
-              );
-            }if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child:  CircularProgressIndicator(color: primaryColor),
-              );
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Capygram',
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        // home: const ResponsiveLayout(
+        //     webScreenLayout: WebScreenLayout(),
+        //     mobileScreenLayout: MobileScreenLayout()),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.hasData) {
+                return const ResponsiveLayout(
+                  webScreenLayout: WebScreenLayout(),
+                  mobileScreenLayout: MobileScreenLayout(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child:  CircularProgressIndicator(color: primaryColor),
+                );
+              }
+              
             }
-            
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child:  CircularProgressIndicator(color: primaryColor),
-              );
-            }
-          return const LoginScreen(); 
-        },
+            if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child:  CircularProgressIndicator(color: primaryColor),
+                );
+              }
+            return const LoginScreen(); 
+          },
+        ),
       ),
     );
   }
