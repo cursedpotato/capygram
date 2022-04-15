2ad0b08e76ddc4a693d41e623d894d733ae731f9
Daniel <danireysan@gmail.com>
2022-04-15 17:28:37 -0500

    added a list for the home screen and changed name of the file

---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,12 @@
+import 'package:capygram/screens/add_post_screen.dart';
+import 'package:flutter/material.dart';
+
+const webScreenSize = 600;
+
+List<Widget> homeScreenItems = [
+  const Text('Home'),
+  const Text('Search'),
+  AddPostScreen(),
+  const Text('Favorite'),
+  const Text('Person')
+];
