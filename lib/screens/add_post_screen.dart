f3e7131bac55a6fb28b51732e40ca3bad7d2cb46
Daniel <danireysan@gmail.com>
2022-04-15 14:11:00 -0500

    added post origin master

---
+++ lib/screens/add_post_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class AddPostScreen extends StatefulWidget {
+  AddPostScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: IconButton(onPressed: () {
+      
+    }, icon: const Icon(Icons.upload)));
+  }
+}
