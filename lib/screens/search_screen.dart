edcfdb33096e5516f361a4f089222919033cffaf
Daniel <danireysan@gmail.com>
2022-04-19 16:12:42 -0500

    created search screen

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,86 @@
+import 'package:capygram/utils/colors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class SearchScreen extends StatefulWidget {
+  SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController searchController = TextEditingController();
+  bool isShowUsers = false;
+  @override
+  void initState() {
+    super.initState();
+    searchController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          decoration: const InputDecoration(labelText: "Search for a user...."),
+          onFieldSubmitted: (String _) {
+            setState(() {
+              isShowUsers = true;
+            });
+          },
+        ),
+      ),
+      body: isShowUsers
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where('username',
+                      isGreaterThanOrEqualTo: searchController.text)
+                  .get(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              snapshot.data!.docs[index]["photoUrl"])),
+                    );
+                  },
+                );
+              },
+            )
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                }
+
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 3,
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) =>
+                      Image.network(snapshot.data!.docs[index]["postUrl"]),
+                  staggeredTileBuilder: (index) => StaggeredTile.count(
+                    (index % 7 == 0 ? 2 : 1),
+                    (index % 7 == 0 ? 2 : 1),
+                  ),
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                );
+              },
+            ),
+    );
+  }
+}
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:capygram/screens/add_post_screen.dart';
 import 'package:capygram/screens/feed_screen.dart';
+import 'package:capygram/screens/search_screen.dart';
 import 'package:flutter/material.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItems = [
   const FeedScreen(),
-  const Text('Search'),
+  SearchScreen(),
   AddPostScreen(),
   const Text('Favorite'),
   const Text('Person')
