24720d351a811ff7a4576e40a7ab8088fca18814
Daniel <danireysan@gmail.com>
2022-04-25 18:24:53 -0500

    capygram is almost working on web

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,16 @@ import 'package:capygram/screens/login_screen.dart';
 import 'package:capygram/utils/colors.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyBphuNlMBK2rhRsXLo1x8J7KT0tz8jYP5k",
-        appId: "capygram-aae3d",
-        messagingSenderId: "101216335639",
-        storageBucket: "capygram-aae3d.appspot.com",
-        projectId: "1:101216335639:web:7ec5614696637da2dca952",
-      ),
-    );
-  } else {}
-  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
