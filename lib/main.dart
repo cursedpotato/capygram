db4b4e2b867b11d86ec49d7adfceda188029ab45
Daniel <danireysan@gmail.com>
2022-04-25 16:27:57 -0500

    trying to get capygram going failed

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,15 @@ import 'package:capygram/screens/login_screen.dart';
 import 'package:capygram/utils/colors.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:capygram/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyBphuNlMBK2rhRsXLo1x8J7KT0tz8jYP5k",
-        authDomain: "capygram-aae3d.firebaseapp.com",
-        projectId: "capygram-aae3d",
-        storageBucket: "capygram-aae3d.appspot.com",
-        messagingSenderId: "101216335639",
-        appId: "1:101216335639:web:40984ea7f9fdec5bdca952",
-      ),
-    );
-  } else {}
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -34,9 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => UserProvider())
-      ],
+      providers: [ChangeNotifierProvider(create: (_) => UserProvider())],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Capygram',
@@ -58,19 +46,19 @@ class MyApp extends StatelessWidget {
                 return Center(
                   child: Text('${snapshot.error}'),
                 );
-              }if (snapshot.connectionState == ConnectionState.waiting) {
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(
-                  child:  CircularProgressIndicator(color: primaryColor),
+                  child: CircularProgressIndicator(color: primaryColor),
                 );
               }
-              
             }
             if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child:  CircularProgressIndicator(color: primaryColor),
-                );
-              }
-            return const LoginScreen(); 
+              return const Center(
+                child: CircularProgressIndicator(color: primaryColor),
+              );
+            }
+            return const LoginScreen();
           },
         ),
       ),
